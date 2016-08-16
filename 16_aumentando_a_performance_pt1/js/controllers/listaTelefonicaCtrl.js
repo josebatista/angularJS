004932cd90570e9f1870308745427a552546270a
José <jose@Mac-mini-de-Jose.local>
2016-08-16 10:20:46 -0300

    Aumentando a Performance - Parte1

---
+++ 16_aumentando_a_performance_pt1/js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,50 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, $filter, contatos, serialGenerator) {
+  $scope.app = "Lista Telefonica";
+  $scope.contatos = contatos.data;
+
+  var init = function() {
+    calcularImpostos($scope.contatos);
+    generateSerial($scope.contatos);
+  };
+  var calcularImpostos = function(contatos) {
+    contatos.forEach(function(contato) {
+      contato.operadora.precoComImposto = calcularImposto(contato.operadora.preco);
+    });
+  };
+  var generateSerial = function(contatos) {
+    contatos.forEach(function(item) {
+      item.serial = serialGenerator.generate();
+    });
+  };
+  $scope.adicionarContato = function(contato) {
+    contato.serial = serialGenerator.generate();
+    contato.data = new Date();
+    contatosAPI.saveContato(contato).success(function(data) {
+      delete $scope.contato;
+      $scope.contatoForm.$setPristine();
+      carregarContatos();
+    }).error(function(data){
+      $scope.message = "Não foi possível adicionar um contato";
+    });
+  };
+  $scope.apagarContatos = function(contatos) {
+    $scope.contatos = contatos.filter(function(contato) {
+      if(!contato.selecionado) return contato;
+    });
+    $scope.verificarContatoSelecionado(contatos);
+  };
+  $scope.verificarContatoSelecionado = function(contatos) {
+    $scope.hasContatoSelecionado = contatos.some(function(contato) {
+      return contato.selecionado;
+    });
+  };
+  $scope.ordenarPor = function(campo){
+    $scope.criterioDeOrdenacao = campo;
+    $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+  };
+  var calcularImposto = function(preco) {
+    var imposto = 1.2;
+    return preco * imposto;
+  };
+  init();
+});
